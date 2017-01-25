95c914bfabd55cd693066e21065a97cffbfb3982
nygeog <daniel.martin.sheehan@gmail.com>
2017-01-25 14:39:36 -0500

    Remove notes

--- 20170127/cartocamp_20170127_data_science_with_maps_01.py
+++ 20170127/cartocamp_20170127_data_science_with_maps_01.py
@@ -43,32 +43,3 @@ df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 print df.head(10)
 
-# 2:30 - 3 pm - Getting setup (CARTO accounts, API keys, programming environment, etc.)
-# HOWTO
-# Fill in credentials
-
-# 3:00 pm - Introduction to maps with data science
-# Spatial data
-# Cool things we've seen
-# Tools?
-# 3:10 pm - Getting setup
-# See 2:30 pm above
-# 3:15 pm - Basics on workflows?
-# Generate SQL query to get the data that you want, stressing that they should only pick a 24 hour period of time OR Import API to directly get custom queried data into their accounts (via URL)
-# Read from Carto (pandas.read_csv and SQL API, format csv)
-# Intersect Taxi Points w/ Admin Geographies, read to DataFrame, Groupby, join to Admin Geog Boundaries
-# Write to disk
-# request.post() to send to CARTO OR Python Client create_vis='True' to create their first map
-# Gain intuition of the data in pandas
-# 3:30 pm - Static and Interactive Maps
-# Python template code for generating Maps with the Maps API
-# Start with generic Carto maps and styles
-# Then show them how to tune the map;
-# Styles
-# TurboCarto
-# Zoom and Center Map
-# 3:40 - Spatial Statistics?
-# Getis-Ord's G*? Moran's I? K-means, etc.
-# Data Observatory to contextualize information
-
-# 3:45 - 4:00 pm - Discussion
